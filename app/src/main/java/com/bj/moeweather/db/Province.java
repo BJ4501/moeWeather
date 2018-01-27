b47145ddfa8e14d8edc0d5b0ced0867aa1fd3291
bj4501 <neko.bj4501@gmail.com>
2018-01-28 00:16:08 +0800

    省市县数据库、表构建、litepal配置

---
+++ app/src/main/java/com/bj/moeweather/db/Province.java
@@ -0,0 +1,40 @@
+package com.bj.moeweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * 省份是实体类
+ * Created by BJ on 2018/1/27.
+ */
+public class Province extends DataSupport{
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
