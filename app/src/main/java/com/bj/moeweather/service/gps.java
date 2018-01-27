c4e2cf97824d1eebc27d213d115b5cfa86094972
BJ4501 <neko.bj4501@gmail.com>
2018-01-27 23:40:24 +0800

    一些依赖，butterknife

---
+++ app/src/main/java/com/bj/moeweather/service/gps.java
@@ -0,0 +1,12 @@
+package com.bj.moeweather.service;
+
+/**
+ * Created by Neko-T4 on 2018/1/24.
+ */
+
+public class gps {
+    //获取定位城市
+    //http://blog.csdn.net/qq_24636637/article/details/50461284
+
+
+}
